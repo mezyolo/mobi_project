2793ffd9c44a713240c6770e7a2bceddf96b8f28
Franck <franck.jorge@hotmail.fr>
2017-02-27 09:38:03 -0500

    Update FileController and MessageController

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,18 +9,21 @@ class FileController extends Controller
 	// TODO : Manage a timestamp for deleting from the File Table
 	// TimeStamp Constant
 
-   	public function create($name, $link)
+   	public function create($name, $link, $size)
    	{
    		// Create entry (with a timestamp)
+         DB::insert('insert into files (name, link, size) values (:name, :link, :size)', ['name' => $name, 'link' => $link, 'size' => $size]);
    	}
 
    	public function fetchById($id)
    	{
    		// Fetch the file to get the link for the download
+         $file = DB::select('select * from files where id = :id', ['id' => $id]);
    	}
 
    	public function deleteOldFiles() {
    		// Delete all the old files from the table where the timestamp is over
+         // $deleted = DB::delete('delete from files');
    	}
 
   	// TODO : Generate random string for the download link
--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,11 +9,14 @@ class MessageController extends Controller
     public function create($content, $date_send, $chat_id, $file_id)
     {
     	// Create entry for message depends the content if it's a string message or a file
+   		DB::insert('insert into messages (content, date_time, chat_id, file_id) values (:content, :date_time, :chat_id, :file_id)', ['content' => $content, 'date_time' => $date_send, 'chat_id' => $chat_id, 'file_id' => $file_id]);
+    	
     }
 
     public function fecthByChat($chat_id)
     {
     	// Fetch all messages from chat id
+         $files = DB::select('select * from messages where chat_id = :chat_id', ['chat_id' => $chat_id]);
     }
 
 }
